a8adee4bcee1a5592217055221a4536c4b583ab6
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-05-22 17:08:34 +0300

    Added kwargs

--- find_forks/find_forks.py
+++ find_forks/find_forks.py
@@ -35,23 +35,23 @@ def determine_names(remote_name=CONFIG['remote_name'], **kwargs):
     return user, repo
 
 
-def git_remote_add(name, url):
+def git_remote_add(name, url, **kwargs):
     """Add fork as git remote."""
     remote_add_cmd = 'git remote add %s %s' % (name, url)
     print(remote_add_cmd)
-    if not CONFIG['dry_run']:
+    if not kwargs.get('dry_run', CONFIG['dry_run']):
         subprocess.call(remote_add_cmd.split(' '))
 
 
-def git_fetch_all():
+def git_fetch_all(**kwargs):
     """Fetch all forks."""
     fetch_all_cmd = 'git fetch --all'
     print(fetch_all_cmd)
-    if not CONFIG['dry_run']:
+    if not kwargs.get('dry_run', CONFIG['dry_run']):
         subprocess.call(fetch_all_cmd.split(' '))
 
 
-def add_forks(url, follow_next=True):
+def add_forks(url, follow_next=True, **kwargs):
     """Add forks to the current project."""
     print('Open %s' % url)
     try:
@@ -64,7 +64,7 @@ def add_forks(url, follow_next=True):
         content = response.read().decode('utf-8')
         forks = json.loads(content)
         for fork in forks:
-            git_remote_add(fork['owner']['login'], fork['clone_url'])
+            git_remote_add(fork['owner']['login'], fork['clone_url'], **kwargs)
         # Gets link to next page.
         if follow_next:
             link = response.getheader('Link', '') if PY3 else dict(response.info()).get('link', '')
@@ -84,9 +84,9 @@ def find_forks(user=None, repo=None, determine_names_handler=None, per_page=100,
 
     url = 'https://api.github.com/repos/%s/%s/forks?per_page=%s' % (user, repo, per_page)
     while url:
-        url = add_forks(url)
+        url = add_forks(url, **kwargs)
 
-    git_fetch_all()
+    git_fetch_all(**kwargs)
 
 
 def main():
@@ -105,13 +105,11 @@ def main():
         help='Specify git remote name to determine user and repo (default: %s)' % (CONFIG['remote_name'], ))
     parser.add_argument('-u', '--user', default=None, help='Specify github user')
     parser.add_argument('-r', '--repo', default=None, help='Specify github user\'s repo')
-    parser.add_argument('--dry-run', action='store_true', help='Do not run the git commands', default=CONFIG['dry_run'])
+    parser.add_argument('--dry-run', default=CONFIG['dry_run'], action='store_true', help='Do not run the git commands')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
     args = parser.parse_args()
 
-    CONFIG['dry_run'] = args.dry_run
-    kwargs = {'remote_name': args.remote_name}
-    find_forks(args.user, args.repo, **kwargs)
+    find_forks(**vars(args))
 
 if __name__ == '__main__':
     main()
