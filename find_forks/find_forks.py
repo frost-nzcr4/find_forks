ea317c1e807c94e22858c6e3092072e7fdc12813
Alexander Pervakov <frost.nzcr4@jagmort.com>
2015-05-22 15:31:33 +0300

    Enabled remote_name argument from console

--- find_forks/find_forks.py
+++ find_forks/find_forks.py
@@ -13,14 +13,17 @@ from six import PY3
 from six.moves import urllib  # pylint: disable=import-error
 
 CONFIG = {
-    'dry_run': False
+    'dry_run': False,
+    'remote_name': 'origin'
 }
 
 
-def determine_names(remote_name=None):
-    """Try to determine the user / repository name."""
-    # A clone from github will use "origin" as remote name by default.
-    remote_cmd = 'git config --get remote.%s.url' % (remote_name if remote_name else 'origin', )
+def determine_names(remote_name=CONFIG['remote_name'], **kwargs):
+    """Try to determine the user and repository name.
+
+    :param remote_name: A clone from github will use "origin" as remote name by default.
+    """
+    remote_cmd = 'git config --get remote.%s.url' % (remote_name, )
     try:
         origin = subprocess.check_output(remote_cmd.split(' ')).decode('utf-8').strip()
         user, repo = origin.split('/')[-2:]
@@ -72,12 +75,12 @@ def add_forks(url, follow_next=True):
     return None
 
 
-def find_forks(user=None, repo=None, determine_names_handler=None, per_page=100):
+def find_forks(user=None, repo=None, determine_names_handler=None, per_page=100, **kwargs):
     """Find forks.
 
     Runs all methods in proper order to find all forks of github user/repo."""
     if user is None and repo is None:
-        user, repo = determine_names() if determine_names_handler is None else determine_names_handler()
+        user, repo = determine_names(**kwargs) if determine_names_handler is None else determine_names_handler(**kwargs)
 
     url = 'https://api.github.com/repos/%s/%s/forks?per_page=%s' % (user, repo, per_page)
     while url:
@@ -97,6 +100,9 @@ def main():
             raise
 
     parser = ArgumentParser(prog='find_forks')
+    parser.add_argument(
+        '-n', '--remote-name', default=CONFIG['remote_name'],
+        help='Specify git remote name to determine user and repo (default: %s)' % (CONFIG['remote_name'], ))
     parser.add_argument('-u', '--user', default=None, help='Specify github user')
     parser.add_argument('-r', '--repo', default=None, help='Specify github user\'s repo')
     parser.add_argument('--dry-run', action='store_true', help='Do not run the git commands', default=CONFIG['dry_run'])
@@ -104,7 +110,8 @@ def main():
     args = parser.parse_args()
 
     CONFIG['dry_run'] = args.dry_run
-    find_forks(args.user, args.repo)
+    kwargs = {'remote_name': args.remote_name}
+    find_forks(args.user, args.repo, **kwargs)
 
 if __name__ == '__main__':
     main()
